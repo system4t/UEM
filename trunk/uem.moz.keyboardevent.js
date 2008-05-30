4271fd5bb9f79f3e645250166cf0f87514b2ec7f
Allan <no@where.com>
2008-05-30 08:28:57 +0000

    Define a TextEvent class for Firefox.
    git-svn-id: https://svn.system4t.com/uem@27 922c6f3e-1335-48c9-a0e4-64a29bb76817

--- trunk/uem.moz.keyboardevent.js
+++ trunk/uem.moz.keyboardevent.js
@@ -84,72 +84,77 @@ if (navigator.product == "Gecko" && navigator.vendor != "Apple Computer, Inc.") 
     },
     true
   );
-//  /**
-//   * Initialize a TextEvent.   Keyword 'this' is a window.event object.
-//   * 
-//   * @param type {String} Event type.
-//   * @param canBubble Boolean that determines if the event propagates.
-//   * @param cancelable Boolean that determines if the event can be cancelled.
-//   * @param view Reference to the view (window).
-//   * @param data A string.
-//  */
-//  TextEvent.prototype.initTextEvent =
-//    function(type, canBubble, cancelable, view, data) {
-//    if (TRACE) EPE.appendTrace("initTextEvent()");
-//    var charCode = data.charCodeAt(0);
-//    var keyCode = charCode;
-//    var iShift = (62 <= keyCode && keyCode <= 90 ||
-//      33 <= keyCode && keyCode <= 42 ||
-//      keyCode == 58 || keyCode == 60 ||
-//      94 <= keyCode && keyCode <= 95 ||
-//      123 <= keyCode && keyCode <= 126 ||
-//      data == "+" || data == ":" || data == "_");
-//    
-//    this.initKeyEvent(type,canBubble, cancelable, view,
-//      false,
-//      false,
-//      iShift,
-//      false, keyCode, charCode);
-//  };
-//    
-//    /**
-//     * Initialize a KeyboardEvent.   Keyword 'this' is a window.event object.
-//     * 
-//     * @param type {String} Event type.
-//     * @param canBubble Boolean that determines if the event propagates.
-//     * @param cancelable Boolean that determines if the event can be cancelled.
-//     * @param view Reference to the view (window).
-//     * @param keyIdentifier Key identifier.
-//     * @param keyLocation Key location. 0 for standard, 1 for left, 2 for right,
-//     *    and 3 for numpad.
-//     * @param modifierList String containing "Alt", "Control", "Meta", and/or "Shift".
-//     */
-//    UEMEvent.prototype.initKeyboardEvent =
-//      function(type, canBubble, cancelable, view, keyIdentifier, keyLocation, modifierList) {
-//      if (TRACE) EPE.appendTrace("initKeyboardEvent()");
-//      var ishift = modifierList.contains(/Shift/);
-//      var keyCode;
-//      if (ishift)
-//        keyCode = UEM.keyIdentifierShiftToKeyCode(keyIdentifier); 
-//      else
-//        keyCode = UEM.keyIdentifierNoShiftToKeyCode(keyIdentifier);
-//      // semicolon or colon
-//      if (keyIdentifier == "U+003B" || keyIdentifier == "U+003A")
-//        keyCode = 59;
-//      // equals or plus
-//      else if (keyIdentifier == "U+003D" || keyIdentifier == "U+002B")
-//        keyCode = 61;
-//      // minus or underscore
-//      else if (keyIdentifier == "U+002D" || keyIdentifier == "U+005F")
-//        keyCode = 109;
-//      var charCode = 0;
-//      this.keyIdentifier = keyIdentifier;
-//      this.keyLocation = keyLocation;
-//      this.initKeyEvent(type,canBubble, cancelable, view,
-//        modifierList.contains(/Control/),
-//        modifierList.contains(/Alt/), 
-//        ishift,
-//        false, keyCode, charCode);
-//    };
+  function TextEvent() {
+    this.data = null;
+  }
+  TextEvent.prototype = new UIEvent();
+
+  /**
+   * Initialize a TextEvent.   Keyword 'this' is a window.event object.
+   * 
+   * @param type {String} Event type.
+   * @param canBubble Boolean that determines if the event propagates.
+   * @param cancelable Boolean that determines if the event can be cancelled.
+   * @param view Reference to the view (window).
+   * @param data A string.
+  */
+  TextEvent.prototype.initTextEvent =
+    function(type, canBubble, cancelable, view, data) {
+    if (TRACE) EPE.appendTrace("initTextEvent()");
+    var charCode = data.charCodeAt(0);
+    var keyCode = charCode;
+    var iShift = (62 <= keyCode && keyCode <= 90 ||
+      33 <= keyCode && keyCode <= 42 ||
+      keyCode == 58 || keyCode == 60 ||
+      94 <= keyCode && keyCode <= 95 ||
+      123 <= keyCode && keyCode <= 126 ||
+      data == "+" || data == ":" || data == "_");
+    
+    this.initKeyEvent(type,canBubble, cancelable, view,
+      false,
+      false,
+      iShift,
+      false, keyCode, charCode);
+  };
+    
+    /**
+     * Initialize a KeyboardEvent.   Keyword 'this' is a window.event object.
+     * 
+     * @param type {String} Event type.
+     * @param canBubble Boolean that determines if the event propagates.
+     * @param cancelable Boolean that determines if the event can be cancelled.
+     * @param view Reference to the view (window).
+     * @param keyIdentifier Key identifier.
+     * @param keyLocation Key location. 0 for standard, 1 for left, 2 for right,
+     *    and 3 for numpad.
+     * @param modifierList String containing "Alt", "Control", "Meta", and/or "Shift".
+     */
+    UEMEvent.prototype.initKeyboardEvent =
+      function(type, canBubble, cancelable, view, keyIdentifier, keyLocation, modifierList) {
+      if (TRACE) EPE.appendTrace("initKeyboardEvent()");
+      var ishift = modifierList.contains(/Shift/);
+      var keyCode;
+      if (ishift)
+        keyCode = UEM.keyIdentifierShiftToKeyCode(keyIdentifier); 
+      else
+        keyCode = UEM.keyIdentifierNoShiftToKeyCode(keyIdentifier);
+      // semicolon or colon
+      if (keyIdentifier == "U+003B" || keyIdentifier == "U+003A")
+        keyCode = 59;
+      // equals or plus
+      else if (keyIdentifier == "U+003D" || keyIdentifier == "U+002B")
+        keyCode = 61;
+      // minus or underscore
+      else if (keyIdentifier == "U+002D" || keyIdentifier == "U+005F")
+        keyCode = 109;
+      var charCode = 0;
+      this.keyIdentifier = keyIdentifier;
+      this.keyLocation = keyLocation;
+      this.initKeyEvent(type,canBubble, cancelable, view,
+        modifierList.contains(/Control/),
+        modifierList.contains(/Alt/), 
+        ishift,
+        false, keyCode, charCode);
+    };
 
 }
