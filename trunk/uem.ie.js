3aba20e9764862dd6a6d6ce13cf48496e9d8b02a
Tavs <no@where.com>
2008-07-07 10:55:39 +0000

    Fixed missing semicolon.
    git-svn-id: https://svn.system4t.com/uem@68 922c6f3e-1335-48c9-a0e4-64a29bb76817

--- trunk/uem.ie.js
+++ trunk/uem.ie.js
@@ -500,14 +500,14 @@ if (document.createEventObject) {
   // Define Event interface for window
   window.addEventListener = UEM.addEventListener;
   window.removeEventListener = UEM.removeEventListener;
-  window.dispatchEvent = UEM.dispatchEvent
+  window.dispatchEvent = UEM.dispatchEvent;
   window.removeAllEventListeners = UEM.removeAllEventListeners;
 
   
   // Define Event interface for document
   document.addEventListener = UEM.addEventListener;
   document.removeEventListener = UEM.removeEventListener;
-  document.dispatchEvent = UEM.dispatchEvent
+  document.dispatchEvent = UEM.dispatchEvent;
   document.removeAllEventListeners = UEM.removeAllEventListeners;
   
   
